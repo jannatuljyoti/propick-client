10f4c04c96ca4ea432f9be7d15666d4d70c90a15
jannatuljyoti <jannatuljyoti085@gmail.com>
2025-06-12 00:01:05 +0600

    added register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { use } from 'react';
+import useDynamicTitle from '../hooks/dynamicTitle';
+
+import { Link, useLocation, useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import { FcGoogle } from 'react-icons/fc';
+import { AuthContext } from '../context/AuthProvider';
 
 const Login = () => {
+     useDynamicTitle("Login")
+
+  const {signIn,googleLogin}=use(AuthContext);
+
+  const location=useLocation();
+  const navigate=useNavigate();
+
+  const handleLogin=(e)=>{
+    e.preventDefault();
+    const form = e.target;
+    const email=form.email.value;
+    const password=form.password.value;
+    // console.log({email,password});
+
+    signIn(email,password)
+    .then((result)=>{
+      const user = result.user;
+      console.log(user);
+      toast.success("Login Successfully");
+      navigate(`${location.state?location.state : "/"}`);
+    })
+    .catch((error)=>{
+      toast.error("Wrong Password");
+    });
+  };
+
+    const handleGoogleLogin=()=>{
+    googleLogin()
+    .then((result)=>{
+      const user = result.user;
+      console.log(user);
+      toast.success("Google Signed in successful!")
+    })
+    .catch((error)=>{
+      toast.error(error.message);
+    });
+  };
     return (
-        <div>
-            Login
+        <div className='flex justify-center   items-center min-h-screen '>
+             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+
+                <h2 className='p-4 font-semibold text-2xl'>Login your account</h2>
+      <form onSubmit={handleLogin} className="card-body">
+        <fieldset className="fieldset">
+
+          <label className="label text-xl ">Email</label>
+          <input name='email' type="email" className="input" placeholder="Email" required/>
+
+          <label className="label text-xl ">Password</label>
+          <input name='password' type="password" className="input" placeholder="Password" required/>
+
+         <Link to="/forget">Forget Password?</Link>
+          
+
+          <button type='submit' className="btn text-white bg-[#4bbafa] mt-4">Login</button>
+
+          <div className='flex justify-center items-center mt-3'>
+           <button onClick={handleGoogleLogin} type='button'  className='btn px-21 py-4 flex items-center gap-2 bg-[#141414] text-white '>Sign up with  <FcGoogle />Google</button>
+           </div>
+
+          <p className='font-semibold text-center mt-4'>Don't Have An Account? <Link to="/auth/register" className='text-[#4bbafa]'>Register</Link></p>
+        </fieldset>
+      </form>
+      <ToastContainer></ToastContainer>
+
+     
+
+    </div>
         </div>
     );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,108 @@
-import React from 'react';
+import React, { use, useState } from 'react';
+import useDynamicTitle from '../hooks/dynamicTitle';
+
+import { Link, useNavigate } from 'react-router';
+import { toast, ToastContainer } from 'react-toastify';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import { updateProfile } from 'firebase/auth';
+import { AuthContext } from '../context/AuthProvider';
 
 const Register = () => {
+      useDynamicTitle("Register")
+    const {createUser,setUser,googleLogin}=use(AuthContext);
+
+    const [showPassword, setShowPassword]=useState(false);
+    const navigate =useNavigate();
+
+    const handleRegister=(e)=>{
+      e.preventDefault();
+        const form=e.target;
+        const name=form.name.value;
+        const photo=form.photo.value;
+        const email=form.email.value;
+        const password=form.password.value;
+        // console.log({name,photo,email,password});
+
+         const upperCase = /[A-Z]/.test(password);
+    const lowerCase = /[a-z]/.test(password);
+
+
+        if(password.length<8 || !upperCase || !lowerCase){
+      toast.error("Password should be at least 8 characters and include uppercase & lowercase letter.");
+      return;
+    }
+
+    createUser(email,password)
+    .then((result)=>{
+        const user=result.user;
+
+      return  updateProfile(user,{
+          displayName:name,
+          photoURL:photo
+        })
+        .then(()=>{
+          setUser({...user,displayName:name, photoURL:photo});
+        toast.success("Registered successfully");
+        navigate("/");
+        })
+    })
+    .catch((error)=>{
+        toast.error(error.message);
+    });
+    };
+
+    const handleGoogleLogin=()=>{
+        googleLogin()
+        .then((result)=>{
+            setUser(result.user);
+            toast.success("Signed in with Google")
+        })
+        .catch((error)=>{
+            toast.error(error.message);
+        });
+    };
+
+
     return (
-        <div>
-            Register
+       <div className='flex justify-center   items-center min-h-screen'>
+             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5 ">
+
+                <h2 className='font-semibold p-4 text-2xl'>Register your account</h2>
+      <form onSubmit={handleRegister} className="card-body">
+        <fieldset className="fieldset">
+
+        <label className="label  text-xl">Name</label>
+        <input name='name' type="text" className="input" placeholder="Your Name" />
+
+        <label className="label  text-xl">Photo URL</label>
+        <input name='photo' type="text" className="input" placeholder="Photo URL" />
+
+          <label className="label  text-xl">Email</label>
+          <input name='email' type="email" className="input" placeholder="Email" required/>
+
+          <label className="label  text-xl">Password</label>
+          <div className='relative'>
+            <input name='password' type={showPassword?'text':'password'} className="input" placeholder="Password" required/>
+
+            <span onClick={()=>setShowPassword(!showPassword)} className='absolute top-3 right-3 cursor-pointer text-xl text-gray-600' >{showPassword ?<FaEye /> :<FaEyeSlash />}</span>
+          </div>
+
+         
+          <button type='submit' className="btn text-white  bg-[#4bbafa] mt-3">Register</button>
+
+         
+        </fieldset>
+      </form>
+
+      <ToastContainer></ToastContainer>
+
+     <div className='flex justify-center items-center'>
+     <button onClick={handleGoogleLogin} className='flex items-center gap-2  bg-[#141414] text-white px-18 py-2'>Sign up with  <FcGoogle />Google</button>
+     </div>
+
+      <p className='font-semibold text-center mt-3'>Already Have An Account? <Link to="/auth/login" className='text-[#4bbafa]'>Login</Link></p>
+    </div>
         </div>
     );
 };
