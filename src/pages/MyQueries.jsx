6195625cb18d047dd21b2a4160ad8f0b55a14a0c
jannatuljyoti <jannatuljyoti085@gmail.com>
2025-06-14 22:24:03 +0600

    added my queries

--- src/pages/MyQueries.jsx
+++ src/pages/MyQueries.jsx
@@ -1,10 +1,118 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from "../firebase/firebase.init";
+import useDynamicTitle from '../hooks/dynamicTitle';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import Loading from './Loading';
 
 const MyQueries = () => {
+    const [user,loading]=useAuthState(auth);
+    const [query,setQuery]=useState([]);
+    const[fetching, setFetching]=useState(true);
+    const navigate= useNavigate();
+
+    useDynamicTitle("My Queries");
+
+    useEffect(()=>{
+        if(!user)return;
+        axios.get(`http://localhost:3000/my-queries?email=${user.email}`)
+        .then(res=>{
+            setQuery(res.data);
+            setFetching(false);
+        })
+        .catch(err=>{
+            console.error(err);
+            toast.error('Failed to load queries');
+            setFetching(false);
+
+        });
+
+    },[user]);
+
+    const handleDelete=id=>{
+        const confirmDelete=window.confirm('Are you sure you want to delete this query?');
+        if(!confirmDelete) return;
+
+        axios.delete(`http://localhost:3000/query/${id}`)
+        .then(res=>{
+            if(res.data.success){
+                setQuery(prev=> prev.filter(q=>q._id !==id));
+                toast.success('Query deleted successfully');
+            }
+        })
+        .catch(err=>{
+            console.error(err);
+            toast.error('Failed to delete ');
+        });
+    };
+
+    if(loading || fetching){
+        return <Loading></Loading>
+    }
+
+    if(!user){
+        return <p>Please log in to view your queries</p>
+    }
+
     return (
-        <div>
-        <Link to="/add-query" className="btn btn-accent">Add New Query</Link>
+        <div className='max-w-7xl mx-auto px-5 py-12'>
+        {/* <Link to="/add-query" className="btn btn-accent">Add New Query</Link> */}
+
+        {/* Banner Section */}
+        <div className='bg-blue-100 p-5 rounded-lg text-center mb-7'>
+            <h2 className='text-3xl  font-bold'>Your Queries</h2>
+
+            <button onClick={()=> navigate('/add-query')}
+                className='mt-4 px-7 py-3 bg-blue-600 text-white rounded hover:bg-blue-700'>Add New Query</button>
+
+        </div>
+
+        {/* my queries */}
+
+        {
+            query.length===0?(
+                <div className='text-center'>
+                    <p className='text-gray-500 mb-4'>You haven't added any queries yet.</p>
+                    <button
+                    onClick={()=>navigate('/add-query')}
+                    className='px-4 py-3 bg-green-600 text-white rounded hover:bg-green-700'>Add a Query</button>
+                </div>
+            ):(<div className='bg-blue-100 p-7 rounded grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+
+                {
+                    query.map(queries=>(
+                        <div key={queries._id} className='bg-white p-5 shadow rounded'>
+                        
+                        <img src={queries.productImage} alt={queries.productName} className='w-full h-40 object-cover rounded mb-3'/>
+
+                        <h3 className='text-xl font-semibold mb-2'>{queries.queryTitle}</h3>
+
+                        <p className='text-gray-600 text-sm mb-2'>Brand: {queries.productBrand}</p>
+
+                        <p>Added on: {new Date(queries.timestamp).toLocaleString()}</p>
+
+
+                        <div className='flex mt-5 justify-between gap-3'>
+
+                            <button onClick={()=>navigate(`/query-details/${queries._id}`)} className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-yellow-600'>View</button>
+
+                            <button onClick={()=>navigate(`/update-query/${queries._id}`)} className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-yellow-600'>Update</button>
+
+                            <button onClick={()=>handleDelete(queries._id)} className='px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600'>Delete</button>
+                        </div>
+
+                        </div>
+                    ))
+                }
+
+
+            </div>
+                
+                
+                )
+        }
 
         </div>
     );
